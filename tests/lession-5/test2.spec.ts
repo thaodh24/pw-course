4b3c9ac13e93794fb9593350b9fbd78a05a126dc
Thao <huongthao241297@gmail.com>
2024-12-21 11:56:54 +0700

    Feat: add test2.spec lession-5

---
+++ tests/lession-5/test2.spec.ts
@@ -0,0 +1,31 @@
+import { test } from '@playwright/test';
+const xpathProductAccess = '//a[contains(text(),"Bài học 2: Product page")]';
+const xpathProductPageHeader = '//h1[contains(text(),"Simple E-commerce")]';
+const addSP1Button = '//button[@data-product-id = "1"]';
+const addSP2Button = '//button[@data-product-id = "2"]';
+const addSP3Button = '//button[@data-product-id = "3"]';
+
+test("Add product on the bassket", async ({ page }) => {
+    await test.step('Naviagate to Simple E-commerce page', async () => {
+        await page.goto("https://material.playwrightvn.com/");
+        await page.locator(`${xpathProductAccess}`).click();
+        await page.locator(`${xpathProductPageHeader}`).isVisible();
+    })
+
+    await test.step('Select and add 2 sp1 on the basket', async () => {
+        await page.locator(`${addSP1Button}`).dblclick();
+        await page.locator(`//td[contains(text(),"Product 1")]`).isVisible();
+    })
+
+    await test.step('Select and add 3 sp2 on the basket', async () => {
+        await page.locator(`${addSP2Button}`).dblclick();
+        await page.locator(`${addSP2Button}`).click();
+        await page.locator(`//td[contains(text(),"Product 2")]`).isVisible();
+    })
+
+    await test.step('Select and add 1 sp3 on the basket', async () => {
+        await page.locator(`${addSP3Button}`).click();
+        await page.locator(`//td[contains(text(),"Product 3")]`).isVisible();
+    })
+
+})
